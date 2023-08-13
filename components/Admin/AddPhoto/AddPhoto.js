c510cff30362b5c5297f1cff2d7373d4bcfa1718
pascalloria <dev.pascalloria@gmail.com>
2023-08-13 16:35:31 +0200

    test : gestion uploade !

--- components/Admin/AddPhoto/AddPhoto.js
+++ components/Admin/AddPhoto/AddPhoto.js
@@ -19,7 +19,7 @@ const AddPhoto = (props) => {
       body.append('folder', folder);
       body.append('name', newName);
       body.append('file', selectFile);
-      const response = await fetch('http://localhost:5000/upload_files', {
+      const response = await fetch('http://api.pascalloria.fr/upload_files', {
         method: 'POST',
         body,
       });
